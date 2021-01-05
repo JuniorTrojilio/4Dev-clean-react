13f2495f2f1b63ffc8a5c392c40ac74415911507
Junior Trojilio <osmar.trojilio@gmail.com>
2021-01-05 11:08:28 -0300

    feat: ensure RemoteAuthentication throws unexpectedError on default cases

--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -1,7 +1,11 @@
 /* eslint-disable no-unused-vars */
 export enum HttpStatusCode {
   noContent = 204,
-  unauthorized = 401
+  unauthorized = 401,
+  badRequest = 400,
+  ok = 200,
+  serverError = 500,
+  notFound = 404
 }
 
 export type HttpResponse = {
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,6 +1,7 @@
 import { HttpPostClient } from '@/data/protocols/http/http-post-client'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
+import { UnexpectedError } from '@/domain/errors/unexcpected-error'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 
 export class RemoteAuthentication {
@@ -18,8 +19,9 @@ export class RemoteAuthentication {
     })
 
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
-      default: return Promise.resolve()
+      default: throw new UnexpectedError()
     }
   }
 }
