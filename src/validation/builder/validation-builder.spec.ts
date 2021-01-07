61d8335d06bb53e2d36814e79ea06db56690cebe
Junior Trojilio <osmar.trojilio@gmail.com>
2021-01-07 01:19:25 -0300

    test: ensure ValidationBuilder returns a list of validations

--- src/validation/builder/validation-builder.spec.ts
+++ src/validation/builder/validation-builder.spec.ts
@@ -1,19 +1,39 @@
 import { EmailValidation, MinLenghtValidation, RequiredFieldValidation } from '@/validation/validators'
 import { ValidationBuilder as sut } from './validation-builder'
+import faker from 'faker'
 
 describe('ValidationBuilder', () => {
   test('should return RequiredFieldValidation', () => {
-    const validations = sut.field('any_field').required().build()
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    const field = faker.database.column()
+    const validations = sut.field(field).required().build()
+    expect(validations).toEqual([new RequiredFieldValidation(field)])
   })
 
   test('should return RequiredFieldValidation', () => {
-    const validations = sut.field('any_field').email().build()
-    expect(validations).toEqual([new EmailValidation('any_field')])
+    const field = faker.database.column()
+    const validations = sut.field(field).email().build()
+    expect(validations).toEqual([new EmailValidation(field)])
   })
 
   test('should return RequiredFieldValidation', () => {
-    const validations = sut.field('any_field').minLenght(5).build()
-    expect(validations).toEqual([new MinLenghtValidation('any_field', 5)])
+    const field = faker.database.column()
+    const lenght = faker.random.number()
+    const validations = sut.field(field).minLenght(lenght).build()
+    expect(validations).toEqual([new MinLenghtValidation(field, lenght)])
+  })
+
+  test('should return a list of validations', () => {
+    const field = faker.database.column()
+    const lenght = faker.random.number()
+    const validations = sut.field(field)
+      .required()
+      .minLenght(lenght)
+      .email()
+      .build()
+    expect(validations).toEqual([
+      new RequiredFieldValidation(field),
+      new MinLenghtValidation(field, lenght),
+      new EmailValidation(field)
+    ])
   })
 })
