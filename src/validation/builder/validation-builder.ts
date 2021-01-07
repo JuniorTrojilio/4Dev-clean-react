4babd2948e1066250b67320ab870340d1f41558e
Junior Trojilio <osmar.trojilio@gmail.com>
2021-01-07 01:08:38 -0300

    feat: ensure ValidationBuilder has a required helper

---
+++ src/validation/builder/validation-builder.ts
@@ -0,0 +1,24 @@
+import { FieldValidation } from '@/validation/protocols'
+import { RequiredFieldValidation } from '@/validation/validators'
+
+export class ValidationBuilder {
+  private readonly fieldName: string
+  private readonly validations: FieldValidation[]
+  private constructor (fieldName: string, validations: FieldValidation[]) {
+    this.fieldName = fieldName
+    this.validations = validations
+  }
+
+  static field (fieldName: string) : ValidationBuilder {
+    return new ValidationBuilder(fieldName, [])
+  }
+
+  required (): ValidationBuilder {
+    this.validations.push(new RequiredFieldValidation(this.fieldName))
+    return this
+  }
+
+  build (): FieldValidation[] {
+    return this.validations
+  }
+}
