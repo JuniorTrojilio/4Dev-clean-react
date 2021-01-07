827c630c866972e97b9ae02fb93b84e76a6a55d9
Junior Trojilio <osmar.trojilio@gmail.com>
2021-01-06 23:23:53 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -8,6 +8,7 @@ export class EmailValidation implements FieldValidation {
   }
 
   validate (value: string): Error {
-    return new InvalidFieldError(this.fieldName)
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return emailRegex.test(String(value).toLowerCase()) ? null : new InvalidFieldError(this.fieldName)
   }
 }
